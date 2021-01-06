041e755c8015707c1112af92a29b661719384a26
david11yf29 <david11yf29@gmail.com>
2021-01-06 19:25:55 +0800

    updated

---
+++ index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(res => {
+  const todo = res.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+  console.log(`
+    The Todo with ID: ${ID}
+    Has a title of: ${title}
+    Is is finished? ${finished}
+  `);
+})
